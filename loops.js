46f966beb661403f8abee30aca786c87dd4e4750
Janice Darikho <janicedarikho@gmail.com>
2017-11-25 08:07:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
-for (let i = 0; i < 25; i++) {
-  console.log(`I am ${i} strange loop.`);
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    console.log(`I am ${i} strange loop.`);
+  }
 }
