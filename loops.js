1882b91545b052a81b9c5f8607d0fe470cfde5f2
Janice Darikho <janicedarikho@gmail.com>
2017-11-25 08:21:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,10 +4,8 @@ function forLoop(array) {
   for (let i = 1; i < 26; i++) {
     array.push(`"I am ${i} strange loop${i === 1 ? '' : 's'}."`);
   }
+  return array;
 }
-
-forLoop(array);
-console.log(array);
 
 let n = 100;
 
